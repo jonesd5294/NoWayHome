116850b1c8680e375af6a364e4697510783a733c
David Jones <jonesd5294@ad.faytechcc.edu>
2023-02-27 20:07:24 -0500

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare Variables
+            string 
+
+            Console.WriteLine("Welcome to NoWayHome....");
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
